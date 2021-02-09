1ce8fa36d17b780f908fdd6dd60a5d1d429d939c
Hugh Bromund <hughbromund44@gmail.com>
2021-02-09 18:01:48 -0500

    added routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,47 +1,19 @@
+import { Route, BrowserRouter as Router } from "react-router-dom";
+
 import Navigation from "./components/Navigation/Navigation";
-import Card from "./components/Card/Card";
-import Button from "./components/Button/Button";
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
 
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <Navigation />
-      <br />
-
-      <Card>
-        <h1>Hello There</h1>
-        <h2>General Kenobi</h2>
-        <h3>Wait a second, wasn't this your plan</h3>
-        <h4>This is where the fun begins</h4>
-        <h5>Don't Try It</h5>
-        <h6>I have the High Ground</h6>
-        <hr />
-        <Button>Hello There</Button>
-      </Card>
-      <br />
-      <Card>
-        <h1>DUBB CLUB</h1>
-        <h2>General Kenobi</h2>
-        <h3>Wait a second, wasn't this your plan</h3>
-        <h4>This is where the fun begins</h4>
-        <h5>Don't Try It</h5>
-        <h6>I have the High Ground</h6>
-        <hr />
-        <Button>Hello There</Button>
-      </Card>
-      <br />
-      <Card>
-        <h1>DUBB CLUB</h1>
-        <h2>General Kenobi</h2>
-        <h3>Wait a second, wasn't this your plan</h3>
-        <h4>This is where the fun begins</h4>
-        <h5>Don't Try It</h5>
-        <h6>I have the High Ground</h6>
-        <hr />
-        <Button>Hello There</Button>
-      </Card>
+      <Router>
+        <Navigation />
+        <Route path="/login" component={Login} />
+        <Route exact path="/" component={Home} />
+      </Router>
     </div>
   );
 }
--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link } from "react-router-dom";
 import logo from "../../assets/LogoWordmarkWhite.png";
 
 import classes from "./Navigation.module.css";
@@ -16,7 +16,7 @@ export default class Navigation extends Component {
           expand="lg"
           sticky="top"
         >
-          <Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">
             <img alt="" src={logo} width="200" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -25,6 +25,9 @@ export default class Navigation extends Component {
               <Nav.Link>Home</Nav.Link>
               <Nav.Link>Dashboard</Nav.Link>
               <Nav.Link>Search</Nav.Link>
+              <Nav.Link as={Link} to="/login">
+                Login
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
