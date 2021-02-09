050da8795b79c9b1327d9735aee51fbe16a0834d
Hugh Bromund <hughbromund44@gmail.com>
2021-02-09 17:07:10 -0500

    added some style to the navigation bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,6 +31,17 @@ function App() {
         <hr />
         <Button>Hello There</Button>
       </Card>
+      <br />
+      <Card>
+        <h1>DUBB CLUB</h1>
+        <h2>General Kenobi</h2>
+        <h3>Wait a second, wasn't this your plan</h3>
+        <h4>This is where the fun begins</h4>
+        <h5>Don't Try It</h5>
+        <h6>I have the High Ground</h6>
+        <hr />
+        <Button>Hello There</Button>
+      </Card>
     </div>
   );
 }
--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -9,19 +9,26 @@ import classes from "./Navigation.module.css";
 export default class Navigation extends Component {
   render() {
     return (
-      <Navbar className={classes.Navigation} variant="dark" expand="lg">
-        <Navbar.Brand>
-          <img alt="" src={logo} width="200" />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse>
-          <Nav>
-            <Nav.Link>Home</Nav.Link>
-            <Nav.Link>Dashboard</Nav.Link>
-            <Nav.Link>Search</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <div className={classes.NavigationWrapper}>
+        <Navbar
+          className={classes.Navigation}
+          variant="dark"
+          expand="lg"
+          sticky="top"
+        >
+          <Navbar.Brand>
+            <img alt="" src={logo} width="200" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse>
+            <Nav>
+              <Nav.Link>Home</Nav.Link>
+              <Nav.Link>Dashboard</Nav.Link>
+              <Nav.Link>Search</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
     );
   }
 }
