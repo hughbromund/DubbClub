d6514150a5260b622fbf6948c8f036c0dd7e8d19
Hugh Bromund <hughbromund44@gmail.com>
2021-02-14 13:24:38 -0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,18 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const path = require("path");
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(require(path.resolve(__dirname, "./routers/router")));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 const dbConnection = require(path.resolve(__dirname, "./database"));
 
 app.listen(port, () => {
-  console.log(`Dubb Club backend listening at http://localhost:${port}`)
-})
+  console.log(`Dubb Club backend listening at http://localhost:${port}`);
+});
+// small change
