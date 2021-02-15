5cfcbf23d283583b58100a1811c65856b0fc14d5
Nathan Ashta <nathanashta@gmail.com>
2021-02-14 20:35:48 -0500

    basic game cards for new page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,9 @@ import Navigation from "./components/Navigation/Navigation";
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import Register from "./components/Register/Register";
+import ExpandedGameInfo from "./components/ExpandedGameInfo/ExpandedGameInfo";
 import "./constants/Constants";
+import { AnimatedSwitch, AnimatedRoute, spring } from "react-router-transition";
 
 import classes from "./App.module.css";
 
@@ -12,7 +14,12 @@ import classes from "./App.module.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
-import { HOME_ROUTE, LOGIN_ROUTE, REGISTER_ROUTE } from "./constants/Constants";
+import {
+  GAME_INFO_ROUTE,
+  HOME_ROUTE,
+  LOGIN_ROUTE,
+  REGISTER_ROUTE,
+} from "./constants/Constants";
 library.add(fab);
 library.add(fas);
 
@@ -24,7 +31,13 @@ function App() {
         <div className={classes.App}>
           <Route exact path={LOGIN_ROUTE} component={Login} />
           <Route exact path={REGISTER_ROUTE} component={Register} />
+
           <Route exact path={HOME_ROUTE} component={Home} />
+          <Route
+            exact
+            path={GAME_INFO_ROUTE + "/:id"}
+            component={ExpandedGameInfo}
+          />
         </div>
       </Router>
     </div>
---
+++ frontend/src/components/ExpandedGameInfo/ExpandedGameInfo.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import Card from "../Card/Card";
+import { Container, Row, Col } from "react-bootstrap";
+import classes from "./ExpandedGameInfo.module.css";
+import BullsLogo from "../../assets/BullsLogoTest.png";
+import KnicksLogo from "../../assets/KnicksLogoTest.png";
+import GameInfoCard from "../GameInfoCard/GameInfoCard";
+
+export default class ExpandedGameInfo extends Component {
+  render() {
+    return (
+      <div>
+        <Container fluid>
+          <Row>
+            <Col>
+              <div className={classes.center}>
+                <GameInfoCard
+                  homeTeam={"NY Knicks"}
+                  awayTeam={"Chicago Bulls"}
+                  gameTime={"Monday, January 1st @ 9:00 EST"}
+                  predictedWinner={"away"}
+                  predictionConfidence={100.0}
+                  awayLogo={BullsLogo}
+                  homeLogo={KnicksLogo}
+                  onClickHandler={null}
+                />
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <div className={classes.center}>
+                <Card>
+                  <h1>More info here...</h1>
+                </Card>
+              </div>
+            </Col>
+            <Col>
+              <div className={classes.center}>
+                <Card>
+                  <h1>More info here...</h1>
+                </Card>
+              </div>
+            </Col>
+            <Col>
+              <div className={classes.center}>
+                <Card>
+                  <h1>More info here...</h1>
+                </Card>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
