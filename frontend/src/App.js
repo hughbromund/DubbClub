e2d2d6aa8e21f745431e01fac95f71d788c3e7d0
Hugh Bromund <hughbromund44@gmail.com>
2021-02-16 12:12:51 -0500

    login is now persisted and automatically refreshed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
-import { Route, BrowserRouter as Router } from "react-router-dom";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import React, { Component } from "react";
+
+import AuthContext from "./contexts/AuthContext";
 
 import Navigation from "./components/Navigation/Navigation";
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import Register from "./components/Register/Register";
+import Dashboard from "./components/Dashboard/Dashboard";
 import "./constants/Constants";
 
 import classes from "./App.module.css";
@@ -16,19 +20,39 @@ import { HOME_ROUTE, LOGIN_ROUTE, REGISTER_ROUTE } from "./constants/Constants";
 library.add(fab);
 library.add(fas);
 
-function App() {
-  return (
-    <div>
-      <Router>
-        <Navigation />
-        <div className={classes.App}>
-          <Route exact path={LOGIN_ROUTE} component={Login} />
-          <Route exact path={REGISTER_ROUTE} component={Register} />
-          <Route exact path={HOME_ROUTE} component={Home} />
-        </div>
-      </Router>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    console.log("App Mounted");
+
+    // When the website starts, we need to see if the user is logged in already or not
+    this.context.verifyLogin();
+  }
+
+  render() {
+    // console.log(this.context.isLoggedIn);
+    return (
+      <div>
+        <Router>
+          <Navigation />
+
+          <div className={classes.App}>
+            <Switch>
+              <Route exact path={HOME_ROUTE} component={Home} />
+              <Route path={LOGIN_ROUTE} component={Login} />
+              <Route path={REGISTER_ROUTE} component={Register} />
+              {this.context.isLoggedIn === true ? (
+                <Route path="/dashboard" component={Dashboard} />
+              ) : (
+                ""
+              )}
+              <Route path="*" component={Login} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    );
+  }
 }
+App.contextType = AuthContext;
 
 export default App;
