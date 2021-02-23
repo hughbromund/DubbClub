dfe4c26a1b6248c75fde5704191331d815316a0a
Hugh Bromund <hughbromund44@gmail.com>
2021-02-23 14:31:04 -0500

    working on making gameInfoCards look better

--- frontend/src/components/GameInfoCard/GameInfoCard.js
+++ frontend/src/components/GameInfoCard/GameInfoCard.js
@@ -6,8 +6,16 @@ import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
 import Button from "../Button/Button";
 import classes from "./GameInfoCard.module.css";
+import Expand from "react-expand-animated";
 
 export default class GameInfoCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      expandInfo: false,
+    };
+  }
   renderButtonConditionally() {
     return this.props.onClickHandler === null ? null : (
       <Button onClick={this.props.onClickHandler}>See More</Button>
@@ -16,58 +24,83 @@ export default class GameInfoCard extends Component {
   render() {
     return (
       <div>
-        <Card>
-          <Container>
-            <Row xs={1} sm={2} md={2} lg={2} xl={2}>
-              <Col>
-                <div
-                  className={[
-                    classes.centered,
-                    classes.verticalCenterImage,
-                  ].join(" ")}
-                >
-                  <img src={this.props.awayLogo} class={classes.logo} />
+        <div className={classes.wrapper}>
+          <Card className={classes.outer}>
+            <Container>
+              <Row xs={1} sm={2} md={2} lg={2} xl={2}>
+                <Col>
+                  <div className={classes.background}>
+                    <div
+                      className={[
+                        classes.centered,
+                        classes.verticalCenterImage,
+                      ].join(" ")}
+                    >
+                      <img src={this.props.awayLogo} class={classes.logo} />
+                    </div>
+                  </div>
+                  <br />
+                  <div className={classes.centered}>
+                    <h3>
+                      <b>{this.props.awayTeam}</b>
+                    </h3>
+                  </div>
+                </Col>
+                <Col>
+                  <div className={classes.background}>
+                    <div
+                      className={[
+                        classes.centered,
+                        classes.verticalCenterImage,
+                      ].join(" ")}
+                    >
+                      <img src={this.props.homeLogo} class={classes.logo} />
+                    </div>
+                  </div>
+                  <br />
+                  <div className={classes.centered}>
+                    <h3>
+                      <b>{this.props.homeTeam}</b>
+                    </h3>
+                  </div>
+                </Col>
+              </Row>
+              <hr />
+              <Row>
+                <div>
+                  <h5>
+                    Predicted Winner:{" "}
+                    <b>
+                      {this.props.predictedWinner === "away"
+                        ? this.props.awayTeam
+                        : this.props.homeTeam}
+                    </b>
+                  </h5>
+                  <h5>
+                    Prediction Confidence:{" "}
+                    <b>{this.props.predictionConfidence}%</b>
+                  </h5>
                 </div>
-                <div className={classes.centered}>
-                  <h2>{this.props.awayTeam}</h2>
-                </div>
-              </Col>
-              <Col>
-                <div
-                  className={[
-                    classes.centered,
-                    classes.verticalCenterImage,
-                  ].join(" ")}
-                >
-                  <img src={this.props.homeLogo} class={classes.logo} />
-                </div>
-                <div className={classes.centered}>
-                  <h2>{this.props.homeTeam}</h2>
-                </div>
-              </Col>
-            </Row>
-            <hr />
-            <Row>
-              <div className={classes.centered}>
-                <h6>Game Time: {this.props.gameTime}</h6>
-                <h3>
-                  Predicted Winner:{" "}
-                  <b>
-                    {this.props.predictedWinner === "away"
-                      ? this.props.awayTeam
-                      : this.props.homeTeam}
-                  </b>
-                </h3>
-                <h4>
-                  Prediction Confidence:{" "}
-                  <b>{this.props.predictionConfidence}%</b>
-                </h4>
-              </div>
-            </Row>
-            <hr />
-            {this.renderButtonConditionally()}
-          </Container>
-        </Card>
+              </Row>
+
+              {/* {this.renderButtonConditionally()} */}
+            </Container>
+          </Card>
+          <div
+            className={classes.gameTime}
+            onMouseEnter={() => {
+              this.setState({ expandInfo: true });
+            }}
+            onMouseLeave={() => {
+              this.setState({ expandInfo: false });
+            }}
+          >
+            <b>{this.props.gameTime}</b>
+            <Expand open={this.state.expandInfo}>
+              <div>Some more content can go here</div>
+            </Expand>
+          </div>
+        </div>
       </div>
     );
   }
