ded45f6967ee368bbb6fca2aed851892b420f409
Hugh Bromund <hughbromund44@gmail.com>
2021-02-25 12:10:49 -0500

    finished upgrade for Game Info Cards

--- frontend/src/components/GameInfoCard/GameInfoCard.js
+++ frontend/src/components/GameInfoCard/GameInfoCard.js
@@ -8,7 +8,17 @@ import Button from "../Button/Button";
 import classes from "./GameInfoCard.module.css";
 import Expand from "react-expand-animated";
 import ReactSpeedometer from "react-d3-speedometer";
+const rgbHex = require("rgb-hex");
+const hexRgb = require("hex-rgb");
 
+/**
+ * This maps the value of a number from one range to a new one.
+ *
+ * Used to map confidence values from range 50-100 to range 0-100 for graphing
+ * */
+Number.prototype.map = function (in_min, in_max, out_min, out_max) {
+  return ((this - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
 export default class GameInfoCard extends Component {
   constructor(props) {
     super(props);
@@ -16,24 +26,42 @@ export default class GameInfoCard extends Component {
     this.state = {
       expandInfo: false,
     };
+
+    this.hexAlphaConverter = this.hexAlphaConverter.bind(this);
+    this.hexMedianValue = this.hexMedianValue.bind(this);
   }
   renderButtonConditionally() {
     return this.props.onClickHandler === null ? null : (
       <Button onClick={this.props.onClickHandler}>See More</Button>
     );
   }
-  render() {
-    var confidenceString = "";
 
-    if (this.props.predictionConfidence <= 25) {
-      confidenceString = "Leaning";
-    } else if (this.props.predictionConfidence <= 50) {
-      confidenceString = "Likely";
-    } else if (this.props.predictionConfidence <= 75) {
-      confidenceString = "Very Likely";
-    } else if (this.props.predictionConfidence <= 100) {
-      confidenceString = "Extremely Likely";
-    }
+  hexAlphaConverter(hexValue, alphaValue) {
+    var rgbValue = hexRgb(hexValue);
+    rgbValue.alpha = alphaValue;
+
+    return (
+      "#" + rgbHex(rgbValue.red, rgbValue.green, rgbValue.blue, rgbValue.alpha)
+    );
+  }
+
+  hexMedianValue(hex1, hex2) {
+    var rgb1 = hexRgb(hex1);
+    var rgb2 = hexRgb(hex2);
+
+    return (
+      "#" +
+      rgbHex(
+        (rgb1.red + rgb2.red) / 2,
+        (rgb1.green + rgb2.green) / 2,
+        (rgb1.blue + rgb2.blue) / 2
+      )
+    );
+  }
+
+  render() {
+    var homeHex = "#E13A3E";
+    var awayHex = "#008248";
 
     return (
       <div>
@@ -88,26 +116,82 @@ export default class GameInfoCard extends Component {
               <Row>
                 <div className={classes.speedometer}>
                   <ReactSpeedometer
-                    value={this.props.predictionConfidence}
-                    minValue={0}
+                    value={
+                      this.props.predictedWinner === "away"
+                        ? this.props.predictionConfidence.map(50, 100, 0, 100) *
+                          -1
+                        : this.props.predictionConfidence.map(50, 100, 0, 100)
+                    }
+                    minValue={-100}
                     maxValue={100}
-                    segments={100}
-                    maxSegmentLabels={4}
+                    segments={7}
                     needleColor={"white"}
                     ringWidth={10}
-                    startColor={"#0050dd"}
-                    endColor={"#1d6efc"}
-                    currentValueText={"${value}% Confidence"}
+                    currentValueText={
+                      Math.abs(this.props.predictionConfidence) + "% Confidence"
+                    }
+                    segmentColors={[
+                      this.hexAlphaConverter(awayHex, 1),
+                      this.hexAlphaConverter(awayHex, 0.6),
+                      this.hexAlphaConverter(awayHex, 0.4),
+                      this.hexAlphaConverter(
+                        this.hexMedianValue(homeHex, awayHex),
+                        0.2
+                      ),
+                      this.hexAlphaConverter(homeHex, 0.4),
+                      this.hexAlphaConverter(homeHex, 0.6),
+                      this.hexAlphaConverter(homeHex, 1),
+                    ]}
+                    customSegmentLabels={[
+                      {
+                        text: "100%",
+                        position: "OUTSIDE",
+                      },
+                      {
+                        text: "",
+                        position: "OUTSIDE",
+                      },
+                      {
+                        text: "",
+                        position: "OUTSIDE",
+                      },
+                      {
+                        text: "50%",
+                        position: "OUTSIDE",
+                      },
+                      {
+                        text: "",
+                        position: "OUTSIDE",
+                      },
+                      {
+                        text: "",
+                        position: "OUTSIDE",
+                      },
+                      {
+                        text: "100%",
+                        position: "OUTSIDE",
+                      },
+                    ]}
                   />
                 </div>
                 <div>
                   <h5>
-                    {confidenceString}{" "}
-                    <b>
-                      {this.props.predictedWinner === "away"
-                        ? this.props.awayTeam
-                        : this.props.homeTeam}
-                    </b>
+                    {this.props.predictionConfidence > 60 ? (
+                      <div>
+                        <b>{this.props.predictionConfidence}%</b> confidence
+                        that the{" "}
+                        <b>
+                          {this.props.predictedWinner === "away"
+                            ? this.props.awayTeam
+                            : this.props.homeTeam}
+                        </b>{" "}
+                        win
+                      </div>
+                    ) : (
+                      <div>
+                        <b>Toss Up</b> Game
+                      </div>
+                    )}
                   </h5>
                 </div>
               </Row>
