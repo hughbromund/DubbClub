2dabb51279b5a5bd9e43c859fc6935b5cec985ac
Daniel Joseph <42781293+dadeej@users.noreply.github.com>
2021-02-28 19:28:08 -0500

    finished game schema

--- backend/database/models/game.js
+++ backend/database/models/game.js
@@ -1,20 +1,36 @@
 const mongoose = require('mongoose')
+const Team = require('./team')
 const Schema = mongoose.Schema
 mongoose.promise = Promise
 
 // Define userSchema
 const gameSchema = new Schema({
     gameId: { type: String, unique: true, required: true},
-    date: { type: String, unique: true, required: true},
-    arena: { type: String, unique: true, required: true},
-    home: { type: mongoose.Schema.Types.ObjectId, ref: 'Team', unique: false, required: true},
-    away: { type: mongoose.Schema.Types.ObjectId, ref: 'Team', unique: false, required: true},
-    teamName: { type: String, unique: true, required: true},
+    date: { type: String, unique: false, required: true},
+    arena: { type: String, unique: false, required: false},
+    home: { type: Team, unique: false, required: true},
+    away: { type: Team, unique: false, required: true},
+    predictedWinner: { type: Number, unique: false, required: false},
+    confidence: { type: Number, unique: false, required: false},
+    gameStats: { 
+        home: {
+            teamId: { type: Number, unique: true, required: true},
+            score: { type: Number, unique: true, required: true},
+            lineScore: { type: Number, unique: true, required: true},
+            leaders: { type: Number, unique: true, required: true}
+        },
+        away: {
+            teamId: { type: Number, unique: true, required: true},
+            score: { type: Number, unique: true, required: true},
+            lineScore: { any: Object, unique: true, required: true},
+            leaders: { any: Object, unique: true, required: true}
+        }
+    }
 }, { collection: "Game"})
 
 // Define schema methods
 gameSchema.methods = {}
 
 
-const Team = mongoose.model('Game', gameSchema)
+const Game = mongoose.model('Game', gameSchema)
 module.exports = Game
