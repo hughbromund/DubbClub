102c2686ef2946d1a9b4c8160e884cd98d72a653
dadeej <joseph71@purdue.edu>
2021-03-30 18:04:15 -0400

    Update NbaUpdateService.js

--- backend/services/NbaUpdateService.js
+++ backend/services/NbaUpdateService.js
@@ -52,7 +52,7 @@ exports.refresh = async function refresh() {
       if (gameInDb.status === "In Play" && upcoming[i].statusGame === "Finished") {
          await NBAgame.findOneAndUpdate({id : gameId}, {status: "Finished"}).exec()
          console.log("Updated game " + gameId + " to Finished.")
-         updateDbWithPlayedGameStats(gameId)
+         let game = updateDbWithPlayedGameStats(gameId)
       }
 
       if (upcoming[i].statusGame === "In Play") {
@@ -63,11 +63,8 @@ exports.refresh = async function refresh() {
    return updatedIds
 }
 
-exports.updateDbWithPlayedGameStats = async function(gameId) {
+updateDbWithPlayedGameStats = async function(gameId) {
    let stats = await nbaService.getPlayedGameStats(gameId)
-   console.log("here")
-   let game = NBAgame.updateOne({ id : gameId }, {playedGameStats : stats}).exec()
-   console.log("here")
-
-   return
+   let game = await NBAgame.updateOne({ id : gameId }, {playedGameStats : stats}).exec()
+   return game
 }
